1ee7b46c4df105777e9cc8514f0cd06352a311c5
Rafa Paradela <rafaparadela@gmail.com>
2014-07-10 17:50:16 +0200

    Initial Commit

---
+++ DemoFirebase/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DemoFirebase
+//
+//  Created by Rafa Paradela on 10/07/14.
+//  Copyright (c) 2014 rafaparadela. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
