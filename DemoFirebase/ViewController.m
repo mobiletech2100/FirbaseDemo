966a143d7b3834f0bc0ea6172a5cdf855a22f120
Rafa Paradela <rafaparadela@gmail.com>
2014-07-12 19:13:44 +0200

    Init Commit

--- DemoFirebase/ViewController.m
+++ DemoFirebase/ViewController.m
@@ -6,9 +6,15 @@
 //  Copyright (c) 2014 rafaparadela. All rights reserved.
 //
 
+#import <Firebase/Firebase.h>
 #import "ViewController.h"
 
 @interface ViewController ()
+
+@property (strong, nonatomic) Firebase * myRootRef;
+@property (weak, nonatomic) IBOutlet UITextField *myTextField;
+@property (weak, nonatomic) IBOutlet UILabel *myLabel;
+
 
 @end
 
@@ -17,13 +23,35 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    [self setupFirebaseOberver];
 }
 
-- (void)didReceiveMemoryWarning
+
+- (Firebase *)myRootRef
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    if (_myRootRef == nil)
+    {
+        _myRootRef = [[Firebase alloc] initWithUrl:@"https://brilliant-fire-5884.firebaseio.com/"];
+        
+    }
+    return _myRootRef;
 }
+
+
+- (IBAction)myTextFieldDidChange:(id)sender {
+    [self.myRootRef setValue:self.myTextField.text];
+}
+
+
+
+- (void)setupFirebaseOberver{
+    [self.myRootRef observeEventType:FEventTypeValue withBlock:^(FDataSnapshot *snapshot) {
+        if(snapshot.value != [NSNull null]) {
+            self.myLabel.text = snapshot.value;
+        }
+    }];
+}
+
+
 
 @end
